1faffdef0e41841a2bbb0c89084f4ee4ed49facb
ledsouza <leandro.souza.159@gmail.com>
2024-06-20 11:51:51 -0300

    implementando estado na barra de pesquisa

--- src/componentes/BarraDePesquisa/index.jsx
+++ src/componentes/BarraDePesquisa/index.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function BarraDePesquisa() {
+    const [termoPesquisa, setTermoPesquisa] = useState("");
     return (
-        <input type="search" placeholder="Digite o que você procura" className="barra-pesquisa" />
+        <input
+            type="search"
+            placeholder="Digite o que você procura"
+            className="barra-pesquisa"
+            value={termoPesquisa}
+            onChange={(evento) => setTermoPesquisa(evento.target.value)}
+        />
     );
 }
